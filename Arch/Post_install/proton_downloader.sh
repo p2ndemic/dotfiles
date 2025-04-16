1465649a98ae577ab8b99910b0e9e156bd04f5a6
p2ndemic <162058065+p2ndemic@users.noreply.github.com>
2025-04-17 04:11:53 +0500

    Update proton_downloader.sh

--- Arch/Post_install/proton_downloader.sh
+++ Arch/Post_install/proton_downloader.sh
@@ -1,4 +1,10 @@
+# Documentation: https://github.com/ValveSoftware/Proton
+
 # - Proton | Wine default dir:
 # .local/share/Steam/compatibilitytools.d
 
+
 # Global variables:
+# PROTON_USE_NTSYNC=1
+# 
+# PROTON_NO_FSYNC=1

5585e7857c227329e9c0d122e0cc2b97c74dc390
p2ndemic <162058065+p2ndemic@users.noreply.github.com>
2025-09-26 02:03:27 +0000

    Update spawn-new-instance.sh

--- .config/sway/scripts/spawn-new-instance.sh
+++ .config/sway/scripts/spawn-new-instance.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Получаем app_id или class focused окна
-app_to_run=$(swaymsg -t get_tree | jq -r '.. | select(.focused? == true) | select(.type == "con") | .app_id // .window_properties.class // empty')
+# Получаем app_id или class активного окна типа "con"
+app_to_run=$(swaymsg -t get_tree | jq -r '.. | select(.focused? == true) | select(.type == "con" or .type == "floating_con") | .app_id // .window_properties.class // empty')
 
-if [ -z "$app_to_run" ] || [ "$app_to_run" = "null" ]; then
-    echo "Cannot determine application to launch"
+if [ -z "$app_to_run" ]; then
+    echo "Cannot determine application to launch" >&2
     exit 1
 fi
 
 # Запускаем новую копию
-exec $app_to_run
+exec "$app_to_run"

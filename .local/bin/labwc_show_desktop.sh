bc5dfd6491e7b5759c3c321915331a3817941e41
p2ndemic <162058065+p2ndemic@users.noreply.github.com>
2026-02-10 04:15:06 +0500

    Update labwc_show_desktop.sh

--- .local/bin/labwc_show_desktop.sh
+++ .local/bin/labwc_show_desktop.sh
@@ -3,8 +3,7 @@
 # Note: Необходимо установить wlrctl
 # paru -S wlrctl
 
-CACHE_DIR="$HOME/.cache"
-CACHE_FILE="$CACHE_DIR/.labwc_show_desktop"
+CACHE_FILE="$HOME/.cache/.labwc_show_desktop"
 
 if wlrctl toplevel find state:unminimized >/dev/null 2>&1; then
     # Сохраняем видимые окна + добавляем фокусированное в конец

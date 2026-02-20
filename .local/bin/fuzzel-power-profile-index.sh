ea8043e7faaadf3f070ba180e813fbbc691a0ce7
p2ndemic <162058065+p2ndemic@users.noreply.github.com>
2026-02-21 02:34:00 +0500

    Update fuzzel-power-profile-index.sh

--- .local/bin/fuzzel-power-profile-index.sh
+++ .local/bin/fuzzel-power-profile-index.sh
@@ -1,95 +1,99 @@
 #!/usr/bin/env bash
 # power-menu — селектор режима питания (tuned-adm + fuzzel --index)
 
-#1. Состояние (state)
-# upower -i /org/freedesktop/UPower/devices/battery_BAT0 | gawk '/state:/ {print $2}'
-#2. Процент заряда (percentage)
-# upower -i /org/freedesktop/UPower/devices/battery_BAT0 | gawk '/percentage:/ {print $2}'
-#3. Полная емкость (energy-full) — округлено до десятых
-# LC_ALL=C upower -i /org/freedesktop/UPower/devices/battery_BAT0 | gawk '/energy-full:/ {printf "%.1f\n", $2}'
-#4. Здоровье аккумулятора (capacity) — округлено до десятых
-# LC_ALL=C upower -i /org/freedesktop/UPower/devices/battery_BAT0 | gawk '/capacity:/ {printf "%.1f\n", $2}'
-#4.1 Здоровье аккумулятора (capacity) — целое число
-# LC_ALL=C upower -i /org/freedesktop/UPower/devices/battery_BAT0 | gawk '/capacity:/ {printf "%.0f\n", $2}'
-# 5. Время до разряда/заряда
-# upower -i /org/freedesktop/UPower/devices/battery_BAT0 | gawk '/time to (empty|full):/ {print $4}'
-
 # === Парсинг батареи ===
+# 1. Состояние (state)
 STATE=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | gawk '/state:/ {print $2}')
+# 2. Процент заряда (percentage
 PERCENT=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | gawk '/percentage:/ {print $2}')
-TIME_TO=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | gawk '/time to (empty|full):/ {print $4}')
+# 3. Время до разряда/заряда
+TIME_TO=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | gawk '/time to (empty|full):/ {print $4 " " $5}')
+# 4. Здоровье аккумулятора (capacity) — округлено до целых
 HEALTH=$(LC_ALL=C upower -i /org/freedesktop/UPower/devices/battery_BAT0 | gawk '/capacity:/ {printf "%.0f\n", $2}')
+# 5. Полная емкость (energy-full) — округлено до десятых
 ENERGY=$(LC_ALL=C upower -i /org/freedesktop/UPower/devices/battery_BAT0 | gawk '/energy-full:/ {printf "%.1f\n", $2}')
 
-if [[ "$STATE" == "Discharging" ]]; then
+if [[ "$STATE" == "discharging" ]]; then
     STATE_ICON="🔋"
+    STATE_ARROW="⇣"
     STATUS="Discharging"
     TIME_LABEL="Time remaining"
 else
     STATE_ICON="🔌"
+    STATE_ARROW="⇡"
     STATUS="Charging"
     TIME_LABEL="Time until full"
 fi
 
-# Сообщение (точно как на твоей картинке)
-MESG="Status ➟ Discharging
-Persent ➟ 72%
-Remaining ➟ 1h 37m
-Health ➟ 90%
-Capacity ➟ 58.7 Wh
-Profile ➟ Balanced
-
-⚡  Режим питания"
-
-
 # Текущий профиль tuned
 CURRENT_PROFILE=$(tuned-adm active 2>/dev/null | gawk '{print $NF}' || echo "balanced")
 
-# Индекс для предвыбора
 case "$CURRENT_PROFILE" in
-    *powersave*) SELECT=0 ;;
-    *balanced*)  SELECT=1 ;;
-    *performance*) SELECT=2 ;;
-    *) SELECT=0 ;;
+    powersave)
+        SELECT=0
+        ;;
+    balanced)
+        SELECT=1
+        ;;
+    performance)
+        SELECT=2
+        ;;
+    *)
+        SELECT=0
+        ;;
 esac
+
+# Сообщение (точно как на твоей картинке)
+MESG="${STATE_ICON} State ⤍ $PERCENT [${STATUS}]
+🕒 Remaining ⤍ ${TIME_TO} ${STATE_ARROW}
+⚡ Capacity ⭬ ${ENERGY} Wh
+🧬 Health ⤏ ${HEALTH}%
+🚀 Profile 🢒 [${CURRENT_PROFILE}]
+
+🚀 Select power profile:"
+
+# К сожалению при вставке эмодзи типа ⚙️ ❤️ в блок --mesg fuzzel падает: https://codeberg.org/dnkl/fuzzel/issues/736
+#  State ➟
+# 󰁫 Remaining  
+#  Health
+#  Capacity
+#  Profile
+#  Select power profile:
+
+
 
 # Формируем список (индекс 0 = Power Saver, индекс 1 = Balanced)
 # Функция выводит пункты меню. Порядок строк определяет их будущий индекс (0, 1, 2...)
 FN_ENTRIES() {
-    echo "    Lock"      # Index [0]
-    echo " 󰗼   Logout"    # Index [1]
-    echo " 󰖔   Suspend"   # Index [2]
-    echo " 󰜉   Reboot"    # Index [3]
-    echo " 󰐥   Shutdown"  # Index [4]
+    echo -e "Power Save\0icon\x1fbattery-caution-symbolic"      # Index [0]
+    echo -e "Balanced\0icon\x1fpreferences-system-power-management"    # Index [1]
+    echo -e "Performance\0icon\x1fspeedometer"   # Index [2]
 }
+
 
 # Запуск fuzzel
 CHOICE=$(FN_ENTRIES | fuzzel --dmenu \
     --index \
-    --select-index=2 \
     --hide-prompt \
-    --mesg "$MESG" \
-    --width=42 \
-    --lines=4 \
-    --icon-theme="Papirus" \
-    --font="JetBrainsMono Nerd Font:size=13.5" \
-    --border-radius=14 \
-    --horizontal-pad=36 \
-    --vertical-pad=22 \
-    --inner-pad=14
-    #--background="1e1e2eff" \
-    #--text-color="cdd6f4ff" \
-    #--selection-color="1e1e2eff" \
-    #--selection-text-color="1e1e2eff" \
-    #--match-color="fab387ff"
+    --select-index=$SELECT \
+    --mesg="$MESG" \
+    --icon-theme="Papirus-Dark" \
+    --font="JetBrainsMono Nerd Font Mono:size=13" \
+    --minimal-lines
     )
+
+    #--select-index=2 \
 
 case "$CHOICE" in
     0)
-        tuned-adm profile powersave || tuned-adm profile laptop-battery-powersave
-        notify-send -i battery-caution-symbolic "Tuned" "Power Saver активирован ✅"
+        exec tuned-adm profile powersave || tuned-adm profile laptop-battery-powersave &
+        notify-send -i battery-caution-symbolic "Tuned" "Power Save активирован ✅"
         ;;
     1)
+        exec tuned-adm profile balanced &
+        notify-send -i preferences-system-power-management "Tuned" "Balanced активирован ✅"
+        ;;
+    2)
         tuned-adm profile balanced
         notify-send -i preferences-system-power-management "Tuned" "Balanced активирован ✅"
         ;;

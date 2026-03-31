352c998e8e2841b311f588270d6f68e45ac22b48
p2ndemic <162058065+p2ndemic@users.noreply.github.com>
2026-03-31 21:33:33 +0500

    Update fuzzel-powermenu.sh

--- .local/bin/fuzzel-powermenu.sh
+++ .local/bin/fuzzel-powermenu.sh
@@ -35,7 +35,7 @@ fuzzel_run() {
         --minimal-lines \
         --width=19 \
         --horizontal-pad=38 \
-        --vertical-pad=20 \
+        --vertical-pad=15 \
         --line-height=38 \
         --selection-radius=8
 }

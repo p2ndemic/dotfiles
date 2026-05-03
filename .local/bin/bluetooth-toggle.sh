607ce57cdda1262bbc3d7db39e8ded82e25e7325
p2ndemic <162058065+p2ndemic@users.noreply.github.com>
2026-05-04 02:55:01 +0500

    Update bluetooth-toggle.sh

--- .local/bin/bluetooth-toggle.sh
+++ .local/bin/bluetooth-toggle.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
+# ══════════════════════════════════════════════════════════════════════
 # Скрипт для переключения состояния Bluetooth (toggle power)
 # Требует bluetoothctl (BlueZ)
 # Необходимо разблокировка модуля через rfkill
 # sudo rfkill unblock bluetooth или sudo rfkill unblock all
 # Добавить текущего пользователя в группу rfkill чтобы не вводить sudo:
 # sudo usermod -aG rfkill $USER
-
-# Проверяем текущее состояние
+# ══════════════════════════════════════════════════════════════════════
+# Проверяем текущее состояние:
 if bluetoothctl show | grep -q "Powered: yes"; then
     bluetoothctl power off
 else
